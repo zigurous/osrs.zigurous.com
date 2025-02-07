5d49a403523ca841eb86c4d55a04800dabb36a77
Adam Graham <adam@zigurous.com>
2025-02-07 16:52:05 -0600

    Add general formatting utils

---
+++ src/utils/formatting.ts
@@ -0,0 +1,15 @@
+export function formatNameFromId(id: string) {
+  return toTitleCase(id.replace(/_/g, ' '));
+}
+
+export function toTitleCase(title: string): string {
+  return title
+    .replace(/(?<!')\b(?!')\w/g, l => l.toUpperCase())
+    .replace(' Of ', ' of ')
+    .replace(' De ', ' de ')
+    .replace('(E)', '(e)');
+}
+
+export function capitalizeFirstLetter(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
