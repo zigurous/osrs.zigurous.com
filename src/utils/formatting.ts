f3a1ab65cd637ab6bd7e5a2b58f9a007032fb7a2
Adam Graham <adam@zigurous.com>
2025-05-25 10:42:14 -0500

    Fix unsupported negative lookbehind in Safari

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -23,7 +23,7 @@ export function toTitleCase(title: string): string {
   }
   return lowercaseParenthesis(
     title
-      .replace(/(?<!')\b(?!')\w/g, l => l.toUpperCase())
+      .replace(/(^\w|\s\w)/g, l => l.toUpperCase())
       .replace(
         /.\b(a|an|and|as|at|but|by|d'|de|en|for|if|illa|in|nor|of|on|or|per|the|to|v.?|vs.?|via)\b/gi,
         l => l.toLowerCase(),
