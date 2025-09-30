948d46aac7b551d90b31c276bd783cf0d11bdb1e
Adam Graham <adam@zigurous.com>
2025-09-30 00:04:09 -0500

    Set default page to /region-analyzer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 
 export default function IndexPage() {
   useEffect(() => {
-    navigate('/grid-master', { replace: true });
+    navigate('/region-analyzer', { replace: true });
   }, []);
   return null;
 }
